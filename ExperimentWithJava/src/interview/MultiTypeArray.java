35cb238d7ab166f88c7feb393158b29e8ca8472e
anupriyanandakumar <anupriyanandakumar@anupriyas-air>
2024-04-30 10:58:10 -0400

    adding new file

--- ExperimentWithJava/src/interview/MultiTypeArray.java
+++ ExperimentWithJava/src/interview/MultiTypeArray.java
@@ -1,9 +1,37 @@
 package interview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MultiTypeArray {
 	
 	/* An input consists of different type elements. ex: [5,6,new Item[3], new Item[new Item[2],4],9] .
 	 *  Evaluate the elements: 5+6+2*3+3*2+2*4+9 
 	 * */
+	
+	public static void main(String[] args) {
+        List<Object> values = new ArrayList<>();
+        values.add(2); // Integer
+        values.add(new Item(5));
+        values.add(new Item(new Item(3)));
+        values.add(4);
+        System.out.println("Input: "+values);
+        addValues(values);
+    }
+
+	private static void addValues(List<Object> values) {
+		String num = "[0-9]";
+		int sum=0;
+		for(Object val: values) {
+			if(val.toString().matches(num)) {
+				sum=sum+(int)val;
+			}
+			else if(val.getClass().isInstance(new Item())) {
+				
+			}
+		}
+		System.out.println("Output: "+sum);
+		
+	}
 
 }
