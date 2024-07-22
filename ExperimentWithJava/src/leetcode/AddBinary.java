503df990cdbdc52cb9e9a15f76baf0afe8189e6f
anupriyanandakumar <anupriyanandakumar@anupriyas-air>
2024-07-22 19:12:12 -0400

    commit

--- ExperimentWithJava/src/leetcode/AddBinary.java
+++ ExperimentWithJava/src/leetcode/AddBinary.java
@@ -12,7 +12,7 @@ public class AddBinary {
 	private static String addBinary(String a, String b) {
 		
 		long result = 0;
-		long tempa = Long.parseLong(a);
+		
 		
 		result = getDecimal(Long.parseLong(a)) + getDecimal(Long.parseLong(b));
 		
